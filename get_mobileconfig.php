d4b92ea00b4dd96fb9f3d57f92122488900555aa
neoarz <164915254+neoarz@users.noreply.github.com>
2025-02-26 19:14:17 -0500

    Update get_mobileconfig.php

--- get_mobileconfig.php
+++ get_mobileconfig.php
@@ -1,16 +1,20 @@
-<?php
-$name_file = "device.mobileconfig";
-if( file_exists($name_file) ) {
-	$file = fopen($name_file, "r");
-	if ($file === false) {
-		echo "Unable to open file!";
-		die;
-	}
-	header("Content-type: application/x-apple-aspen-config; chatset=utf-8");
-	header("Content-Disposition: attachment; filename=\"$name_file\"");
-	echo fread($file,filesize($name_file));
-}
-    else{
-	    echo "File Not Found.";
-	    die;
+// Get filename from URL parameter
+$name_file = isset($_GET['file']) ? $_GET['file'] : "device.mobileconfig";
+
+// Basic security check to prevent directory traversal
+$name_file = basename($name_file);
+
+if(file_exists($name_file)) {
+    $file = fopen($name_file, "r");
+    if ($file === false) {
+        echo "Unable to open file!";
+        die;
     }
+    header("Content-type: application/x-apple-aspen-config; charset=utf-8");
+    header("Content-Disposition: attachment; filename=\"$name_file\"");
+    echo fread($file, filesize($name_file));
+    fclose($file);
+} else {
+    echo "File Not Found.";
+    die;
+}
